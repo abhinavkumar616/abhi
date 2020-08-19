59e38bab6683c0104fc39f1ab7a42be4b4093c8f
unknown <abhinavkumar616@gmail.com>
2020-08-19 04:19:08 -0700

    pattern program

---
+++ pattern programm/src/com/pattern/Pattern8.java
@@ -0,0 +1,33 @@
+package com.pattern;
+
+public class Pattern8 {
+	
+	public static void main(String args[])
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=4;j>=i;j--)
+			{
+				System.out.print(" ");
+			}
+			for(int k=1;k<(i*2);k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int k=9;k>(i*2);k--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ pattern programm/src/com/pattern/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Abhinav
+ *
+ */
+package com.pattern;
